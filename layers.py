79f558d002f66ec36c7b6fd02509da4bfb7ae8f2
Rinigus <rinigus.git@gmail.com>
2017-11-05 12:21:08 +0200

    update layers scripts and example

--- layers.py
+++ layers.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2.7
 
+MAX_ZOOM = 14
+
 import sys, copy, os, codecs
 from yaml import load as yload
 try:
@@ -15,7 +17,7 @@ if len(sys.argv) != 2:
     sys.exit(-1)
 
 finname = sys.argv[1]
-foutname = os.path.splitext(finname)[0] + ".xml"
+foutname = os.path.abspath(os.path.splitext(finname)[0] + ".xml")
 
 print "Reading:", finname
 print "Writing:", foutname
@@ -28,11 +30,12 @@ parameters = definitions["parameters"]
 
 xml = {}
 # zoom levels [0..14]
-for Z in range(14):
+for Z in range(MAX_ZOOM + 1):
 
     print "Z: %2d" % Z, " - ",
 
-    head = etree.Element('Map', srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
+    head = etree.Element('Map',
+                         srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
 
     # parameters
     par = etree.SubElement(head, "Parameters")
@@ -49,9 +52,11 @@ for Z in range(14):
         zmax = layer.get("zmax", defaults["zmax"])
         zmin = layer.get("zmin", defaults["zmin"])
         if Z < zmin or Z > zmax: continue
-        
+        bsize = str(layer.get("buffer-size", defaults["buffer-size"]))
+
         print name,
         l = etree.SubElement(head, "Layer", name=name, srs=srs)
+        l.attrib["buffer-size"] = bsize
         ds = etree.SubElement(l, "Datasource")
 
         ll = copy.copy(defaults)
@@ -60,35 +65,49 @@ for Z in range(14):
         keys = ll.keys()
         keys.sort()
         for par in keys:
-            if par in ["id", "srs", "zmin", "zmax"]: continue
+            if par in ["id", "srs", "zmin", "zmax", "buffer-size"]: continue
             if ll["type"] != "sqlite" and par in ["use_spatial_index"]: continue
 
             p = etree.SubElement(ds, "Parameter", name=par)
             txt = unicode(ll[par])
             if par == "table": txt = "(" + txt + ") AS data"
-            template = Template(txt)        
+            template = Template(txt)
             p.text = template.render(z=Z)
 
     xml[Z] = """<?xml version='1.0' encoding='utf-8'?>
     <!DOCTYPE Map>
-    """ + etree.tostring(head, pretty_print=True)
+    """ + etree.tostring(head, pretty_print=True).replace("ZLEVEL", str(Z))
     print
 
 kk = xml.keys()
 kk.sort()
+xml_range = {}
 for Z1 in kk:
     if Z1 not in xml: continue
-    for Z2 in kk:
-        if Z1 == Z2 or Z2 not in xml: continue
+    Z2 = Z1+1
+    while Z2 in xml:
         if xml[Z1] == xml[Z2]:
             del xml[Z2]
+        else:
+            break
+        Z2 += 1
+    Z2 -= 1
+    xml_range[Z1] = [Z1,Z2]
 
-for Z in xml:
-    outFile = codecs.open(foutname + "-" + str(Z), mode="w", encoding="utf-8")
-    outFile.write(xml[Z])
+cmds = "#!/bin/bash\n\n# Automatically generated script by %s\n\n" % sys.argv[0]
+kk = xml.keys()
+kk.sort()
+for z in kk:
+    Z = xml_range[z]
+    fname = "%s-%s-%s" % (foutname, Z[0], Z[1])
+    outFile = codecs.open(fname, mode="w", encoding="utf-8")
+    outFile.write(xml[z])
     outFile.close()
-    # outFile.write("""<?xml version='1.0' encoding='utf-8'?>
-    # <!DOCTYPE Map>
-    # """)
-    # etree.ElementTree(head).write(outFile, pretty_print=True)
-    # outFile.close()
+    cmds += "echo\necho Processing levels %d -- %d\n" % (Z[0], Z[1])
+    cmds += "tilelive-copy --minzoom=%s --maxzoom=%s --scheme=pyramid bridge://%s $* \n" % (Z[0], Z[1], fname)
+
+fimportername = foutname + "-importer.sh"
+fout = open( fimportername, "w" )
+fout.write(cmds)
+
+print "Importer script at", fimportername
