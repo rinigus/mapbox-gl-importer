675f25bc6194c53d3b30e0566d9630f71bf19f12
rinigus <rinigus.git@gmail.com>
2017-12-28 19:46:30 +0200

    update pois

--- layers/poi/update.sql
+++ layers/poi/update.sql
@@ -90,7 +90,6 @@ UPDATE osm_poi_point SET
 "class" = CASE
 WHEN amenity IN ('clinic', 'dentist', 'doctors', 'hospital', 'nursing_home', 'pharmacy', 'veterinary') THEN 'health'
 WHEN amenity IN ('arts_centre', 'cinema', 'theatre') OR tourism IN ('artwork', 'gallery') THEN 'art'
-WHEN amenity IN ('bank') THEN 'bank'
 WHEN amenity IN ('bar', 'bbq', 'biergarten', 'cafe', 'fast_food', 'food_court', 'ice_cream', 'pub', 'restaurant') THEN 'restaurant'
 WHEN amenity IN ('bicycle_rental', 'bus_station', 'car_rental', 'ferry_terminal', 'taxi') OR
                  highway IN ('bus_stop') OR
@@ -101,13 +100,13 @@ WHEN amenity IN ('college', 'kindergarten', 'school', 'university') OR
                   building IN ('college', 'kindergarten', 'school', 'university')
                   THEN 'school'
 WHEN amenity IN ('bank', 'community_centre', 'courthouse', 'embassy', 'fire_station', 'grave_yard', 'library', 'place_of_worship', 
-                 'post_office', 'shelter', 'telephone', 'toilets', 'townhall') OR
+                 'post_office', 'ranger_station', 'recycling', 'shelter', 'telephone', 'toilets', 'townhall') OR
                  historic IN ('castle', 'monument') OR
                  landuse IN ('basin', 'cemetery', 'reservoir') OR
                  leisure IN ('dog_park', 'garden', 'marina', 'park', 'playground', 'water_park') OR
                  man_made IN ('lighthouse') OR
                  -- churches should be included via place_of_worship, religion is not needed separately
-                 tourism IN ('attraction', 'information', 'museum', 'picnic_site', 'theme_park', 'viewpoint', 'zoo') OR
+                 tourism IN ('attraction', 'aquarium', 'information', 'museum', 'picnic_site', 'theme_park', 'viewpoint', 'zoo') OR
                  waterway <> ''
                  THEN 'public'
 WHEN amenity IN ('police', 'prison') OR barrier IN ('border_control') THEN 'police'
@@ -134,12 +133,12 @@ WHEN amenity IN ('marketplace') THEN 'shop'
 WHEN amenity IN ('post_office') THEN 'post'
 WHEN amenity IN ('swimming_pool') OR leisure IN ('swimming_area', 'swimming_pool', 'water_park') THEN 'swimming'
 WHEN amenity IN ('university') OR building IN ('university') THEN 'college'
-WHEN amenity='place_of_worship' AND religion IN ('christian', 'jewish', 'muslim') THEN 'religious_' || religion
-WHEN barrier IN ('border_control') THEN 'police'
+WHEN amenity='place_of_worship' AND religion IN ('buddhist', 'christian', 'jewish', 'muslim') THEN 'religious_' || religion
+WHEN barrier IN ('border_control') THEN 'barrier'
 WHEN amenity IN ('bank', 'bar', 'bbq', 'bicycle_rental', 'cafe', 'charging_station', 'cinema', 'college',
                  'community_centre', 'courthouse', 'dentist', 'doctors', 'embassy', 'fast_food', 'fire_station',
                  'fuel', 'hospital', 'ice_cream', 'library', 'parking', 'pharmacy', 'place_of_worship', 'police',
-                 'prison', 'restaurant', 'school', 'shelter', 'telephone', 'theatre', 'toilets',
+                 'prison', 'ranger_station', 'recycling', 'restaurant', 'school', 'shelter', 'telephone', 'theatre', 'toilets',
                  'townhall', 'veterinary') THEN amenity
 WHEN building IN ('college', 'school') THEN building
 WHEN historic IN ('castle', 'monument') THEN historic
@@ -153,10 +152,11 @@ WHEN shop IN ('alcohol', 'beverages', 'wine') THEN 'shop_alcohol'
 WHEN shop IN ('bicycle') THEN 'bicycle_rental'
 WHEN shop IN ('books') THEN 'library'
 WHEN shop IN ('doityourself', 'hardware') THEN 'shop_diy'
-WHEN shop IN ('fishmonger', 'seafood') THEN 'shop_seafood'
+WHEN shop IN ('fishmonger') THEN 'shop_seafood'
 WHEN shop IN ('ice_cream') THEN 'ice_cream'
-WHEN shop IN ('art', 'bag', 'bakery', 'coffee', 'convenience', 'deli', 'department_store', 
-              'garden_centre', 'pet', 'toys', 'sports') THEN 'shop_' || shop
+WHEN shop IN ('art', 'bag', 'bakery', 'clothes', 'coffee', 'convenience', 'deli', 'department_store', 
+              'florist', 'garden_centre', 'gift', 'hairdresser', 'laundry', 'music', 'pet', 'seafood',
+              'toys', 'sports') THEN 'shop_' || shop
 WHEN sport IN ('cycling') THEN 'bicycle'
 WHEN sport IN ('horse_racing') THEN 'horse_riding'
 WHEN sport IN ('american_football', 'baseball', 'basketball', 'cricket', 'golf',
@@ -166,7 +166,7 @@ WHEN tourism IN ('bed_and_breakfast', 'guest_house', 'hostel', 'hotel', 'motel')
 WHEN tourism IN ('caravan_site') THEN 'camp_site'
 WHEN tourism IN ('chalet') THEN 'home'
 WHEN tourism IN ('gallery') THEN 'art_gallery'
-WHEN tourism IN ('attraction', 'camp_site', 'information', 'museum', 'picnic_site', 'zoo') THEN tourism
+WHEN tourism IN ('attraction', 'aquarium', 'camp_site', 'information', 'museum', 'picnic_site', 'zoo') THEN tourism
 
 -- specified cases without symbols
 WHEN amenity IN ('nursing_home') OR landuse IN ('basin', 'reservoir', 'sports_centre') OR
