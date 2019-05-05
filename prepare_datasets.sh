1308a76d307113b27a626b165c672afbfd388f99
Rinigus <rinigus.git@gmail.com>
2019-05-05 07:42:39 +0300

    fix typo

--- prepare_datasets.sh
+++ prepare_datasets.sh
@@ -11,7 +11,7 @@ rm $IMPORT_DATA_DIR/water-polygons-split-3857.zip $IMPORT_DATA_DIR/simplified-wa
 wget -P $IMPORT_DATA_DIR https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
 unzip -oj $IMPORT_DATA_DIR/water-polygons-split-3857.zip -d $IMPORT_DATA_DIR
 wget -P $IMPORT_DATA_DIR https://osmdata.openstreetmap.de/download/simplified-water-polygons-split-3857.zip
-unzip -oj $IMPORT_DATA_DIR/simplified-water-polygons-complete-3857.zip -d $IMPORT_DATA_DIR
+unzip -oj $IMPORT_DATA_DIR/simplified-water-polygons-split-3857.zip -d $IMPORT_DATA_DIR
 
 # natural earth
 #wget -P $IMPORT_DATA_DIR http://naciscdn.org/naturalearth/packages/natural_earth_vector.sqlite.zip
